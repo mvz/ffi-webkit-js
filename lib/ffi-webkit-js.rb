412abd71d7c02cf56f6f8d997d5a8f304571d9aa
Matijs van Zuijlen <matijs@matijs.net>
2011-11-23 21:29:50 +0100

    Load WebKit and JS.

---
+++ lib/ffi-webkit-js.rb
@@ -0,0 +1,4 @@
+require 'gir_ffi'
+require 'JS'
+
+GirFFI.setup :WebKit, "1.0"
---
+++ test/integration/load_test.rb
@@ -0,0 +1,8 @@
+require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
+
+describe "Upon load" do
+  it "WebKit and JS should be loaded" do
+    WebKit
+    JS
+  end
+end
